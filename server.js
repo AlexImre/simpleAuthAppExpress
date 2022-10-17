b71e6cb45871fddbf24815d90d4ca5252489d5eb
Alex Imre <alexandre.imre@gmail.com>
2022-10-17 17:48:58 +0100

    set up simple auth app in node express

---
+++ server.js
@@ -0,0 +1,119 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const db = require('./queries');
+const bcrypt = require('bcrypt');
+const passport = require('passport');
+const flash = require('express-flash')
+const session = require('express-session')
+const methodOverride = require('method-override');
+
+
+const initalizePassport = require('./passport-config');
+initalizePassport(
+    passport, 
+    email => users.find(user => user.email === email),
+    id => users.find(user => user.id === id)
+);
+
+
+const users = [];
+
+const app = express();
+const port = 3000;
+
+app.set('view-engine', 'ejs');
+
+app.use(bodyParser.json())
+app.use(
+    bodyParser.urlencoded({
+        extended: true
+    })
+);
+app.use(flash());
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(methodOverride('_method'))
+
+app.get('/', checkAuthenticated, (req, res) => {
+    res.render('index.ejs', { name: req.user.name });
+});
+
+app.get('/login', checkNotAuthenticated, (req, res) => {
+    res.render('login.ejs')
+});
+
+app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: 'login',
+    failureFlash: true
+}));
+
+app.get('/register', checkNotAuthenticated, (req, res) => {
+    res.render('register.ejs')
+});
+
+app.post('/register', checkNotAuthenticated, async (req, res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        users.push({
+            id: Date.now().toString(),
+            name: req.body.name,
+            email: req.body.email,
+            password: hashedPassword
+        })
+        res.redirect('/login')
+    } catch {
+        res.redirect('/register')
+    }
+    console.log(users);
+});
+
+
+
+
+
+// PSQL STUFF
+app.get('/users', db.getUsers);
+app.get('/users/:id', db.getUserById);
+app.post('/users', db.createUser);
+app.put('/users/:id', db.updateUser);
+app.delete('/users/:id', db.deleteUser);
+
+
+app.delete('/logout', (req, res, next) => {
+    req.logOut((err) => {
+        if (err) {
+            return next(err);
+        }
+        res.redirect('/login')
+    })
+})
+
+function checkAuthenticated(req, res, next) {
+    if (req.isAuthenticated()){
+        return next()
+    }
+
+    res.redirect('/login')
+}
+
+function checkNotAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return res.redirect('/')
+    }
+    next()
+}
+
+app.listen(port, () => {
+    console.log(`App running on port ${port}.`);
+});
+
